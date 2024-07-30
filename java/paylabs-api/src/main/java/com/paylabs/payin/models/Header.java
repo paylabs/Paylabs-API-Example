365bc7970809f17fe91be189d3790d447c7356dd
IrfanMaulana00 <satoeirfan@gmail.com>
2024-07-30 10:25:31 +0700

    get merchantId from ENV

--- java/paylabs-api/src/main/java/com/paylabs/payin/models/Header.java
+++ java/paylabs-api/src/main/java/com/paylabs/payin/models/Header.java
@@ -5,11 +5,11 @@ import java.util.Map;
 
 public class Header {
 
-  public static Map<String, String> create(String date, String signature, String merchantId, String requestId) {
+  public static Map<String, String> create(String date, String signature, String requestId) {
     Map<String, String> jsonHeader = new HashMap<>();
     jsonHeader.put("X-TIMESTAMP", date);
     jsonHeader.put("X-SIGNATURE", signature);
-    jsonHeader.put("X-PARTNER-ID", merchantId);
+    jsonHeader.put("X-PARTNER-ID", System.getenv("merchantId"));
     jsonHeader.put("X-REQUEST-ID", requestId);
     jsonHeader.put("Content-Type", "application/json;charset=utf-8");
 
